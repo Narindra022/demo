a6b7821d77edbfa5a930e41a79e21ce5b927b1f1
narindra <acenarindra77@gmail.com>
2023-02-10 16:57:08 +0300

    test_configuration

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,31 @@
 
 namespace App\Controllers;
 
+use App\Models\PromotionModel;
+
 class Home extends BaseController
 {
     public function index()
     {
-        return view('welcome_message');
+        
+        $model_promotion = new PromotionModel();
+    
+        // insert
+            // $donnees = ['intitule' => "Bac 1"];
+            // $model_promotion -> insert($donnees);
+        //find/findAll
+            // $promotion = $model_promotion->findAll();
+            // var_dump($promotion);
+        //update
+            // $donnees = ['intitule'=>'G1'];
+            // $model_promotion->update(4,$donnees);
+        //delete
+        //         $model_promotion->delete(4);
+        // //find/findAll
+        //     $promotion = $model_promotion->findAll();
+        //     var_dump($promotion);
+
+
+        return view('test_view');
     }
 }
---
+++ app/Models/PromotionModel.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class PromotionModel extends Model
+{
+    protected $table      = 'promotion';
+    protected $primaryKey = 'id';
+
+    protected $useAutoIncrement = true;
+
+    protected $returnType     = 'array';
+    protected $useSoftDeletes = true;
+
+    protected $allowedFields = ['intitule'];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'date_creation';
+    protected $updatedField  = 'date_modification';
+    protected $deletedField  = 'date_suppression';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+}
